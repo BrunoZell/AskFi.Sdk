b6befe0e2c56c38285b09a27ced62b7676141be7
Bruno Zell <contact@brunozell.com>
2023-03-24 17:45:49 +0100

    return full observations on perspective queries

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -47,15 +47,15 @@ type Query<'Parameters, 'Result> = 'Parameters -> WorldState -> 'Result
 type IWorldState =
     /// Get the latest received perception of the requested type.
     /// Returns `None` if no observation of the requested type has been made yet.
-    abstract member latest<'Perception> : unit -> 'Perception option
+    abstract member latest<'Perception> : unit -> Observation<'Perception> option
 
     /// Get an iterator the all Observations of type `'Perception` since the passed `timestamp`
     /// (as determined by the runtime clock used during WorldEventStream sequencing).
-    abstract member since<'Perception> : timestamp: DateTime -> 'Perception seq
+    abstract member since<'Perception> : timestamp: DateTime -> Observation<'Perception> seq
 
     /// Todo: get an ordered sequenced of multiple Perception-types
     /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since (as by the runtime clock used for WorldEventStream sequencing) the passed `timestamp`.
-    abstract member since<'Perception1, 'Perception2> : timestamp: DateTime -> System.ValueTuple<'Perception1 option, 'Perception2 option> seq
+    abstract member since<'Perception1, 'Perception2> : timestamp: DateTime -> System.ValueTuple<Observation<'Perception1> option, Observation<'Perception2> option> seq
 
 // ######################
 // ####   STRATEGY   ####
