1269de561d7f04750cfe6db6cc2f10aabedf6a43
Bruno Zell <contact@brunozell.com>
2023-03-30 16:15:30 +0200

    define serialization

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -3,6 +3,26 @@ open System.Collections.Generic
 open System.Runtime.CompilerServices
 open System
 
+// #####################
+// #### PERSISTENCE ####
+// #####################
+
+type EncodedIdea = {
+    /// Content-id of the idea. This is a hash of this ideas 'Content' and is used throughout
+    /// the system to uniquely identify this idea in a content-addressed manner.
+    Cid: byte ReadOnlyMemory // Todo: Make this an IPFS CID respecting multihash and multicodec
+
+    /// Serialized data of the idea. Can be deserialized into an instance of the original 'Idea-type.
+    Content: byte ReadOnlyMemory
+}
+
+type Serializer =
+    abstract member serialize<'Idea> : 'Idea -> EncodedIdea
+    abstract member deserialize<'Idea> : EncodedIdea -> 'Idea option
+
+type Idea =
+    abstract member serializer : Serializer
+
 // ######################
 // #### OBSERVATIONS ####
 // ######################
