5646a7e5aa520afca71415cf22e17a873419d750
Bruno Zell <contact@brunozell.com>
2023-03-30 20:52:06 +0200

    define cid

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -7,10 +7,15 @@ open System
 // #### PERSISTENCE ####
 // #####################
 
+[<IsReadOnly; Struct>]
+type ContentId =
+    // Todo: Make this an IPFS CID respecting multihash and multicodec
+    ContentId of byte ReadOnlyMemory
+
 type EncodedIdea = {
     /// Content-id of the idea. This is a hash of this ideas 'Content' and is used throughout
     /// the system to uniquely identify this idea in a content-addressed manner.
-    Cid: byte ReadOnlyMemory // Todo: Make this an IPFS CID respecting multihash and multicodec
+    Cid: ContentId
 
     /// Serialized data of the idea. Can be deserialized into an instance of the original 'Idea-type.
     Content: byte ReadOnlyMemory
