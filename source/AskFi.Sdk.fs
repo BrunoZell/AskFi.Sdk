26245d9a5573f4ebe07b566a6a4f56260b524fc7
Bruno Zell <contact@brunozell.com>
2023-04-01 13:10:23 +0200

    seperate out persistence types

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -2,29 +2,7 @@ module AskFi.Sdk
 open System.Collections.Generic
 open System.Runtime.CompilerServices
 open System
-
-// #####################
-// #### PERSISTENCE ####
-// #####################
-
-[<IsReadOnly; Struct>]
-type ContentId = {
-    // Todo: Make this an IPFS CID respecting multihash and multicodec
-    Raw: byte ReadOnlyMemory
-}
-
-type EncodedIdea = {
-    /// Content-id of the idea. This is a hash of this ideas 'Content' and is used throughout
-    /// the system to uniquely identify this idea in a content-addressed manner.
-    Cid: ContentId
-
-    /// Serialized data of the idea. Can be deserialized into an instance of the original 'Idea-type.
-    Content: byte ReadOnlyMemory
-}
-
-type Serializer =
-    abstract member serialize<'Idea> : 'Idea -> EncodedIdea
-    abstract member deserialize<'Idea> : EncodedIdea -> 'Idea
+open AskFi.Persistence
 
 // ######################
 // #### OBSERVATIONS ####
