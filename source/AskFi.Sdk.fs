6e557218e4ca4ba4bb1815dd7e850bcc33a9c8d0
Bruno Zell <contact@brunozell.com>
2023-07-20 00:52:27 +0200

    adjust context queries

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -49,15 +49,15 @@ type CapturedObservation<'Percept> = {
 type IContextQueries = 
     /// Get the latest received perception of the requested type.
     /// Returns `None` if no observation of the requested type has been made yet.
-    abstract member latest<'Perception> : unit -> CapturedObservation<'Perception> option
+    abstract member latest<'Percept> : unit -> CapturedObservation<'Percept> option
     
-    /// Get an iterator the all Observations of type `'Perception` since the passed `timestamp`
+    /// Get an iterator the all Observations of type `'Perception` since the passed `from` until `to`
     /// (as determined by the runtime clock used during context sequencing).
-    abstract member since<'Perception> : timestamp: System.DateTime -> CapturedObservation<'Perception> seq
+    abstract member inTimeRange<'Percept> : from: System.DateTime * ``to``: System.DateTime -> CapturedObservation<'Percept> seq
 
-    /// Todo: get an ordered sequenced of multiple Perception-types
-    /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since (as by the runtime clock used for context sequencing) the passed `timestamp`.
-    abstract member since<'Perception1, 'Perception2> : timestamp: System.DateTime -> System.ValueTuple<CapturedObservation<'Perception1> option, CapturedObservation<'Perception2> option> seq
+    /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since the passed `from` until `to``.
+    /// (as by the runtime clock used for context sequencing)
+    abstract member inTimeRange<'Percept1, 'Percept2> : from: System.DateTime * ``to``: System.DateTime -> System.ValueTuple<CapturedObservation<'Percept1> option, CapturedObservation<'Percept2> option> seq
 
 [<IsReadOnly; Struct>]
 type Context = {
