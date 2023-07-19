54e1f61c40ad4f2cbbdb30e18ba6c89949dc8da6
Bruno Zell <contact@brunozell.com>
2023-07-19 12:51:45 +0200

    add context queries

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -32,7 +32,18 @@ type Interpreter<'Percept, 'Identity, 'Reference when 'Identity: comparison> =
 
 /// Public query interface into a given Context.
 /// Used by strategies, visualizations, and standalone analysis code to retrieve information from a Context.
-type IContextQueries = interface end
+type IContextQueries = 
+    /// Get the latest received perception of the requested type.
+    /// Returns `None` if no observation of the requested type has been made yet.
+    abstract member latest<'Perception> : unit -> Observation<'Perception> option
+
+    /// Get an iterator the all Observations of type `'Perception` since the passed `timestamp`
+    /// (as determined by the runtime clock used during WorldEventStream sequencing).
+    abstract member since<'Perception> : timestamp: System.DateTime -> Observation<'Perception> seq
+
+    /// Todo: get an ordered sequenced of multiple Perception-types
+    /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since (as by the runtime clock used for WorldEventStream sequencing) the passed `timestamp`.
+    abstract member since<'Perception1, 'Perception2> : timestamp: System.DateTime -> System.ValueTuple<Observation<'Perception1> option, Observation<'Perception2> option> seq
 
 [<IsReadOnly; Struct>]
 type Context = {
