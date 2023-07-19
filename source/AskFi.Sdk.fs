5a03afefa92bf76c22633801c57f0f76a11728a4
Bruno Zell <contact@brunozell.com>
2023-07-19 20:56:16 +0200

    captured observation for query interface

--- source/AskFi.Sdk.fs
+++ source/AskFi.Sdk.fs
@@ -7,7 +7,7 @@ open System.Threading.Tasks
 // #### OBSERVATIONS ####
 // ######################
 
-/// An atomic appearance of sensory information.
+/// An atomic appearance of sensory information from within an IObserver<'Percept>.
 /// Includes a Percept, which is a typed (via a domain model) representation of the newly observed information.
 /// An observation can have multiple Percepts in case they all appeared at the same instant (point in time).
 /// An individual observation, by definition, appeared at a singular instant (point in time)
@@ -30,20 +30,34 @@ type Interpreter<'Percept, 'Identity, 'Reference when 'Identity: comparison> =
 // #### QUERY ####
 // ###############
 
+/// An atomic appearance of sensory information from outside of IContextQueries.
+/// Includes a Percept, which is a typed (via a domain model) representation of the newly observed information.
+/// An observation can have multiple Percepts in case they all appeared at the same instant (point in time).
+/// An individual observation, by definition, appeared at a singular instant (point in time).
+/// When this observation was captured, a runtime timestamp was attached to later filter it by query-provided unobserved timestamps.
+[<IsReadOnly; Struct>]
+type CapturedObservation<'Percept> = {
+    /// Sensory information captured by the producing IObserver.
+    Percepts: 'Percept array
+
+    /// Runtime timestamp at which this observation was produced by the IObserver.
+    At: System.DateTime
+}
+
 /// Public query interface into a given Context.
 /// Used by strategies, visualizations, and standalone analysis code to retrieve information from a Context.
 type IContextQueries = 
     /// Get the latest received perception of the requested type.
     /// Returns `None` if no observation of the requested type has been made yet.
-    abstract member latest<'Perception> : unit -> Observation<'Perception> option
-
+    abstract member latest<'Perception> : unit -> CapturedObservation<'Perception> option
+    
     /// Get an iterator the all Observations of type `'Perception` since the passed `timestamp`
-    /// (as determined by the runtime clock used during WorldEventStream sequencing).
-    abstract member since<'Perception> : timestamp: System.DateTime -> Observation<'Perception> seq
+    /// (as determined by the runtime clock used during context sequencing).
+    abstract member since<'Perception> : timestamp: System.DateTime -> CapturedObservation<'Perception> seq
 
     /// Todo: get an ordered sequenced of multiple Perception-types
-    /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since (as by the runtime clock used for WorldEventStream sequencing) the passed `timestamp`.
-    abstract member since<'Perception1, 'Perception2> : timestamp: System.DateTime -> System.ValueTuple<Observation<'Perception1> option, Observation<'Perception2> option> seq
+    /// Get an iterator the all Observations of the two types `'Perception1` and `'Perception2` since (as by the runtime clock used for context sequencing) the passed `timestamp`.
+    abstract member since<'Perception1, 'Perception2> : timestamp: System.DateTime -> System.ValueTuple<CapturedObservation<'Perception1> option, CapturedObservation<'Perception2> option> seq
 
 [<IsReadOnly; Struct>]
 type Context = {
